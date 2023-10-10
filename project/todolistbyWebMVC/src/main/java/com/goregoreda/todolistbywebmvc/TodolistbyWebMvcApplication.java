d7d70547a10d6905613ba23968655b79a140eab0
rlarudgkswkd <rlarudgkswkd@gmail.com>
2023-10-10 21:51:03 +0900

    비교용 webmvc 프로젝트 생성

---
+++ project/todolistbyWebMVC/src/main/java/com/goregoreda/todolistbywebmvc/TodolistbyWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.goregoreda.todolistbywebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistbyWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistbyWebMvcApplication.class, args);
+	}
+
+}

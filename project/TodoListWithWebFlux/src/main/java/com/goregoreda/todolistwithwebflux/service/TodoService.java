272eaacc50489cfab4b230706c2f60306c1027de
rlarudgkswkd <rlarudgkswkd@gmail.com>
2023-10-10 20:28:45 +0900

    project structure setting with GPT

--- project/TodoListWithWebFlux/src/main/java/com/goregoreda/todolistwithwebflux/service/TodoService.java
+++ project/TodoListWithWebFlux/src/main/java/com/goregoreda/todolistwithwebflux/service/TodoService.java
@@ -1,4 +1,13 @@
 package com.goregoreda.todolistwithwebflux.service;
 
+import com.goregoreda.todolistwithwebflux.model.Todo;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface TodoService {
+    Mono<Todo> createTodo(Todo todo);
+
+    Flux<Todo> getAllTodos();
+
+    Mono<Todo> getTodoById(String id);
 }
--- project/TodoListWithWebFlux/src/main/java/com/goregoreda/todolistwithwebflux/service/impl/TodoServiceImpl.java
+++ project/TodoListWithWebFlux/src/main/java/com/goregoreda/todolistwithwebflux/service/impl/TodoServiceImpl.java
@@ -1,4 +1,35 @@
 package com.goregoreda.todolistwithwebflux.service.impl;
 
-public class TodoServiceImpl {
+import com.goregoreda.todolistwithwebflux.model.Todo;
+import com.goregoreda.todolistwithwebflux.repository.TodoRepository;
+import com.goregoreda.todolistwithwebflux.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class TodoServiceImpl implements TodoService {
+
+    private final TodoRepository todoRepository;
+
+    @Override
+    public Mono<Todo> createTodo(Todo todo) {
+        // 비즈니스 로직 구현
+        return null;
+    }
+
+    @Override
+    public Flux<Todo> getAllTodos() {
+        // 비즈니스 로직 구현
+        return null;
+    }
+
+    @Override
+    public Mono<Todo> getTodoById(String id) {
+        // 비즈니스 로직 구현
+        return null;
+    }
+
 }

9a097ac5cef20e1125a701d0a6870204d89af93f
rlarudgkswkd <rlarudgkswkd@gmail.com>
2023-10-19 14:57:47 +0900

    람다식 연습

--- project/reactorStudy/src/main/java/com/example/reactorstudy/ReactorStudyApplication.java
+++ project/reactorStudy/src/main/java/com/example/reactorstudy/ReactorStudyApplication.java
@@ -1,5 +1,9 @@
 package com.example.reactorstudy;
 
+import java.util.Arrays;
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
@@ -9,11 +13,24 @@ public class ReactorStudyApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ReactorStudyApplication.class, args);
-    Flux
-        .just(1,2,3,4,5,6)
-        .filter(n -> n % 2 == 0 )
-        .map(n -> n *2)
-        .subscribe(System.out::println);
+    List<Task> tasks = Arrays.asList(Task.builder().name("dd")
+        .component("dd").build(),Task.builder().name("d22d")
+        .component("d22d").build());
+
+    String korBtc = "비트코인";
+
+    tasks.stream()
+        .filter(d -> d.name.startsWith("d"))
+        .map(d -> d.getName() + "(" + korBtc + ")" )
+        .forEach(cc -> System.out.println(cc));
+  }
+
+
+  @Builder
+  @Data
+  public static class Task{
+    String name;
+    String component;
   }
 
 }
